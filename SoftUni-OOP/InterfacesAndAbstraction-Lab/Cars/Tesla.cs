438f0411076a84fd6b3968629de863f20afe9e63
Renal Ahmedov <renalahmedov4@gmail.com>
2021-11-04 23:10:31 +0200

    adding InterfacesAndAbstraction-Lab

---
+++ SoftUni-OOP/InterfacesAndAbstraction-Lab/Cars/Tesla.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar, ICar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Battery = battery;
+            Model = model;
+            Color = color;
+        }
+
+        public int Battery { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+             => "Engine start";
+
+        public string Stop()
+            => "Breaaak!";
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} Tesla {Model} with {Battery} Batteries");
+            sb.AppendLine($"{Start()}");
+            sb.Append($"{Stop()}");
+            return sb.ToString();
+        }
+    }
+}
