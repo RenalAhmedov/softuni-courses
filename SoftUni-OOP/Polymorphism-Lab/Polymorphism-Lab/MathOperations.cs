787600b6af1724fa23ce149544f2696f597760ad
Renal Ahmedov <renalahmedov4@gmail.com>
2021-11-05 17:30:51 +0200

    uploading Polymorphism-Lab-solution

---
+++ SoftUni-OOP/Polymorphism-Lab/Polymorphism-Lab/MathOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public double Add(double double1, double double2, double double3)
+        {
+            return double1 + double2 + double3;
+        }
+        public decimal Add(decimal decimal1, decimal decimal2, decimal decimal3)
+        {
+            return decimal1 + decimal2 + decimal3;
+        }
+    }
+}
---
+++ SoftUni-OOP/Polymorphism-Lab/Shapes/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rectangle = new Rectangle(4.50, 6.50);
+            Shape cirle = new Circle(4.50);
+
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.Draw());
+
+            Console.WriteLine(cirle.CalculateArea());
+            Console.WriteLine(cirle.CalculatePerimeter());
+            Console.WriteLine(cirle.Draw());
+        }
+    }
+}
