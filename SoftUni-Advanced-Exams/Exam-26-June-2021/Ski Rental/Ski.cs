8237ce7fa8d6d89c012591e2fc28c06f2f0ef61f
Renal Ahmedov <renalahmedov4@gmail.com>
2021-12-23 00:06:59 +0200

    exams

---
+++ SoftUni-Advanced-Exams/Exam-26-June-2021/Ski Rental/Ski.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Numerics;
+using System.Collections;
+
+namespace SkiRental
+{
+    class Ski
+    {
+        public Ski(string manufacturer, string model, int year)
+        {
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Manufacturer} - {Model} - {Year}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
