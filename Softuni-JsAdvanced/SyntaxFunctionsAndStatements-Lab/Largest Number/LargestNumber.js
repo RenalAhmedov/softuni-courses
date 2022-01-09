ae334478fc047d2c2b57a0fad02b5d8347cbf409
Renal Ahmedov <renalahmedov4@gmail.com>
2022-01-09 20:30:47 +0200

    uploading solutions

---
+++ Softuni-JsAdvanced/SyntaxFunctionsAndStatements-Lab/Largest Number/LargestNumber.js
@@ -0,0 +1,6 @@
+function biggestNumbers(num1, num2, num3){
+    let biggest = Math.max(num1, num2, num3);
+    console.log(`The largest number is ${biggest}.`)
+}
+// biggestNumbers(5, -3, 16);
+// biggestNumbers(-3, -5, -22.5);
