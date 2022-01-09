15318570e289b70b876417a76440ef79239bddb4
Renal Ahmedov <renalahmedov4@gmail.com>
2022-01-09 20:56:27 +0200

    uploading basics solutions

---
+++ SoftUni-ProgrammingBasics/Conditional-Statements-Advanced-Exercise/09.Voleyball/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Voleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yearType = Console.ReadLine();
+            double holidays = int.Parse(Console.ReadLine());
+            double weekendsInHomeTown = int.Parse(Console.ReadLine());
+
+            double weekends = 48;
+            double holidayGames = holidays * 2 / 3;
+            double sofiaGames = (weekends - weekendsInHomeTown) * 3 / 4;
+
+            double totalGames = holidayGames + sofiaGames + weekendsInHomeTown;
+
+            switch (yearType)
+            {
+                case "leap":
+                    totalGames *= 1.15;
+                    break;
+            }
+
+            Console.WriteLine($"{Math.Floor(totalGames)}");
+        }
+    }
+}
