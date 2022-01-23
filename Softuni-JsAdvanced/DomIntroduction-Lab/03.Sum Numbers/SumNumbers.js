1641976a3ab3866921cbe350ebbd3b58f7b41935
Renal Ahmedov <renalahmedov4@gmail.com>
2022-01-23 18:04:53 +0200

    uploading DomIntroduction lab solutions

---
+++ Softuni-JsAdvanced/DomIntroduction-Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    // TODO: sum = num1 + num2
+    let firstNumber = Number(document.getElementById('num1').value)
+    let secondNumber = Number(document.getElementById('num2').value)
+    document.getElementById('sum').value = firstNumber + secondNumber;
+}
