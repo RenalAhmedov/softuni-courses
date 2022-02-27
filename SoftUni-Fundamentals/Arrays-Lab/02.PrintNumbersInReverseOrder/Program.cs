1d4bf8e441681a96789fd6a2db81d7b0e40d5b55
Renal Ahmedov <renalahmedov4@gmail.com>
2022-02-27 23:44:54 +0200

    uploading fundamentals solutions

---
+++ SoftUni-Fundamentals/Arrays-Lab/02.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.PrintNumbersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] numbers = new string[n];
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Console.ReadLine();
+            }
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+
+
+        }
+    }
+}
