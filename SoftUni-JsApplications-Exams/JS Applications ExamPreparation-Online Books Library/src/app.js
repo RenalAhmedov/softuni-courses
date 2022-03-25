7a6be79a4a11f193dede026d2ba218d31fb8b56c
Renal Ahmedov <renalahmedov4@gmail.com>
2022-03-25 23:49:03 +0200

    uploading exam practice

---
+++ SoftUni-JsApplications-Exams/JS Applications ExamPreparation-Online Books Library/src/app.js
@@ -0,0 +1,53 @@
+import { logout } from './api/api.js';
+import { page, render } from './lib.js';
+import { getUserData } from './util.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { myBooksPage } from './views/my.books.js';
+import { registerPage } from './views/register.js';
+
+const root = document.getElementById('site-content');
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
+
+page(decorateContext);
+page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage)
+page('/create', createPage)
+page('/details/:id', detailsPage)
+page('/edit/:id', editPage )
+page('/my-books', myBooksPage)
+
+updateUserNav(); 
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
+
+    next();
+}
+
+function onLogout () {
+    logout();
+    updateUserNav();
+    page.redirect('/');
+}
+
+function updateUserNav() {
+    const userData = getUserData();
+
+    if(userData) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+        document.querySelector('#user span').textContent = `Welcome, ${userData.email}`;
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
+
+
---
+++ SoftUni-JsApplications-Exams/JS Applications ExamPreparation-Online Books Library/src/views/register.js
@@ -0,0 +1,58 @@
+import { register } from '../api/api.js';
+import { html } from '../lib.js';
+
+const registerTemplate = (onSubmit) => html`
+<section id="register-page" class="register">
+    <form @submit=${onSubmit}id="register-form" action="" method="">
+        <fieldset>
+            <legend>Register Form</legend>
+            <p class="field">
+                <label for="email">Email</label>
+                <span class="input">
+                    <input type="text" name="email" id="email" placeholder="Email">
+                </span>
+            </p>
+            <p class="field">
+                <label for="password">Password</label>
+                <span class="input">
+                    <input type="password" name="password" id="password" placeholder="Password">
+                </span>
+            </p>
+            <p class="field">
+                <label for="repeat-pass">Repeat Password</label>
+                <span class="input">
+                    <input type="password" name="confirm-pass" id="repeat-pass" placeholder="Repeat Password">
+                </span>
+            </p>
+            <input class="button submit" type="submit" value="Register">
+        </fieldset>
+    </form>
+</section>`;
+
+
+export function registerPage(ctx) {
+    ctx.render(registerTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+        const repass = formData.get('confirm-pass').trim();
+
+
+        if (email == '' || password == '' || repass == '') {
+            return alert('All fields are required!');
+        }
+        if (password != repass) {
+            return alert('Passwords don\'t match!');
+        }
+
+        await register(email, password);
+        ctx.updateUserNav();
+        ctx.page.redirect('/');
+
+    }
+}
