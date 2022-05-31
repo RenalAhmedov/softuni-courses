ddb7fde0e00fe68f886a35c00ede747d4b401265
Renal Ahmedov <renalahmedov4@gmail.com>
2022-05-31 17:22:36 +0300

    uploading functions and procedures solutions

---
+++ SoftUni-DB/Functions-Procedures/03. Town Names Starting With.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE usp_GetTownsStartingWith @Name NVARCHAR(MAX)
+AS
+BEGIN
+	SELECT [Name] 
+	 FROM  [Towns] 
+	WHERE  [Name] LIKE @Name + '%'
+END
+
+
---
+++ SoftUni-DB/Functions-Procedures/10. People with Balance Higher Than.sql
@@ -0,0 +1,11 @@
+CREATE PROC usp_GetHoldersWithBalanceHigherThan (@number MONEY)
+AS
+BEGIN
+	SELECT [FirstName], [LastName] 
+	FROM [AccountHolders] 
+	WHERE [Id] IN (SELECT [AccountHolderId] 
+		FROM [Accounts]		
+		GROUP BY [AccountHolderId] 
+		HAVING SUM([Balance]) > @number)
+    ORDER BY [FirstName], [LastName]	
+END
